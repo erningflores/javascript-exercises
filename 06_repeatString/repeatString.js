febd601c3add3e00ba8411921e1e2ed0ba6b168e
erningflores <146033433+erningflores@users.noreply.github.com>
2025-07-07 05:26:29 +0800

    Odin exercise 06
    I just followed the instruction.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
 
+    let ctr = 0;
+    let result = "";
+    while(ctr < num){
+        result += str;
+        ctr++;
+    }
+    console.log(result);
+    return result;
 };
 
 // Do not edit below this line
