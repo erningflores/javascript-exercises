2e737684fe675458101b499bb306972dae2286e2
erningflores <146033433+erningflores@users.noreply.github.com>
2025-07-07 05:51:13 +0800

    Odin exercise 07
    I just followed the instrunction.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const result = str.split("");
+    const arr = result.reverse();
+    str = arr.join("");
+    console.log(`${str} ${result} ${arr} ${str}`);
+    return str;
 };
 
 // Do not edit below this line
