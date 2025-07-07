d2fcaf7a386065a1e118bab88e14b99cd1cd5ac6
erningflores <146033433+erningflores@users.noreply.github.com>
2025-07-07 08:45:06 +0800

    Odin exercise 10
    I just followed the instruction.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if(num % 100 === 0 ){
+        if(num % 400 === 0){
+            return true;
+        }
+        return false;
+    }else if(num % 4 === 0){
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
