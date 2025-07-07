4841804f571c2913808ba84bf03e896d1c1061c9
erningflores <146033433+erningflores@users.noreply.github.com>
2025-07-07 08:19:05 +0800

    Odin exercise 09
    I just followed the instruction.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(m, n) {
+    let sum = 0, low, high;
 
+    if(!Number.isInteger(m) || !Number.isInteger(n)){
+        return "ERROR";
+    }else if(m < 0 || n < 0){
+        return "ERROR";
+    }else if(m < n){
+        low = m;
+        high = n;
+    }else {
+        low = n;
+        high = m;
+    }
+
+    for(let i=low; i<=high; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
